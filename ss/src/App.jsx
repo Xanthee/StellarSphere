db4e33f9a62d930082f207b1733bf6db5a51ac2a
Xanthe <116354535+Xanthee@users.noreply.github.com>
2025-05-05 14:37:27 +0100

    adding anv bar logo and spline

--- ss/src/App.jsx
+++ ss/src/App.jsx
@@ -1,14 +1,41 @@
 import EarthCarousel from './components/EarthCarousel';
 import VenusCarousel from './components/VenusCarousel';
 import StarCarousel from './components/StarCarousel';
+import Spline from '@splinetool/react-spline';
+import Logo from './assets/Logo.png'
 
 const Example = () => {
   return (
-    <div className="bg-neutral-800">
-      <div className="flex h-screen items-center justify-center">
-        <span className="font-semibold uppercase text-neutral-500">
-          Scroll down
-        </span>
+    <div className="bg-black">
+      <div className="flex h-screen flex-col">
+        <nav className=" p-4 flex justify-between items-center">
+         <div>
+            <img
+              src={Logo}
+              alt="Logo"
+              className="h-24 w-auto object-contain"
+            />
+          </div>
+          <div className="flex space-x-4">
+            <button className="font-orbitron font-black text-white">
+              Button A
+            </button>
+            <button className="font-orbitron font-black text-white">
+              Button B
+            </button>
+            <button className="font-orbitron font-black text-white ">
+              Button C
+            </button>
+          </div>
+        </nav>
+        <div className="relative flex-1">
+          <div className="absolute inset-0 w-full h-full">
+            <Spline scene="https://prod.spline.design/6yf5VWqCg3DxPGtL/scene.splinecode" />
+          </div>
+          <span className="absolute inset-0 font-orbitron text-8xl font-bold uppercase text-white flex pt-12 justify-center z-10 drop-shadow-[0px_0px_34px_rgba(223,209,255,1)]">
+            Stellar Sphere
+          </span>
+        </div>
       </div>
       <EarthCarousel />
       <div className="flex h-48 h-screen items-center justify-center">
