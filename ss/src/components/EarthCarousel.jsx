e3594e808a1c15e257c72117eeaaa1b40b4d3704
Xanthe <116354535+Xanthee@users.noreply.github.com>
2025-05-24 13:13:07 +0100

    changing from position absolute to flex

--- ss/src/components/EarthCarousel.jsx
+++ ss/src/components/EarthCarousel.jsx
@@ -9,12 +9,12 @@ const HorizontalScrollCarousel = () => {
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({ target: targetRef });
 
-  const x = useTransform(scrollYProgress, [0, 1], ['0%', '-66.66%']);
+  const x = useTransform(scrollYProgress, [0, 1], ['0%', '-40.66%']);
   const yEarth = useTransform(scrollYProgress, [0, 1], [0, 200]); 
   const yEarthB = useTransform(scrollYProgress, [0, 1], [0, 150]);  
-  const yMoon = useTransform(scrollYProgress, [0, 1], [0, -300]);  
+  const yMoon = useTransform(scrollYProgress, [0, 1], [0, -200]);  
   return (
-    <section ref={targetRef} className="relative h-[200vh] bg-black-900">
+    <section ref={targetRef} className="relative h-[150vh] bg-black-900">
       <ParticleBackground/>
       <div className="sticky top-0 flex h-screen items-center overflow-hidden">
         <motion.div style={{ x }} className="flex w-[300vw] flex-col items-center">
@@ -22,11 +22,12 @@ const HorizontalScrollCarousel = () => {
           <div className="flex w-[300vw]">
             <div className=" w-[300vw] h-[100vh] p-24 flex flex-row relative">
               {/* Earth info A */}
-              <div className="flex flex-row w-[70vw] items-center">
+              <div className="flex flex-row w-[30vw] items-center">
                 <motion.div style={{ y: yEarth }}>
                   <img src={Earth} className="" />
                 </motion.div>
-                <div className="card ml-12 flex flex-col">
+              </div>      
+              <div className="card ml-12 flex flex-col">
                   <h1 className="text-white text-xl">
                     4 billion years ago
                   </h1>
@@ -34,20 +35,21 @@ const HorizontalScrollCarousel = () => {
                     The surface was still extremely hot, with volcanic activity, molten rock, and frequent asteroid impacts.
                   </p>
                 </div>
-              </div>      
               {/* Earth info B */}
-              <div className="absolute bottom-1/3 left-[85vw] flex flex-row w-[70vw] items-center">
-                <motion.div style={{ y: yEarthB }}>
-                  <img src={Earth} className="h-[400px] w-[400px]" />
+
+                <div className="flex flex-row w-[30vw] items-center">
+                  <motion.div style={{ y: yEarthB }}>
+                    <img src={Earth} className="h-[400px] w-[400px] ml-12" />
+                  </motion.div>
+                </div>
+                <motion.div
+                  className="w-[200px] h-[400px]"
+                  style={{ y: yMoon }}
+                >
+                  <Spline scene="https://prod.spline.design/LzhVNa1synAwhP-C/scene.splinecode" />
                 </motion.div>
-              </div>
-              <motion.div
-                className="absolute bottom-0 left-1/4 transform w-[200px] h-[400px]"
-                style={{ y: yMoon }}
-              >
-                <Spline scene="https://prod.spline.design/LzhVNa1synAwhP-C/scene.splinecode" />
-              </motion.div>
-              <div className="card absolute left-[40vw] ml-12 flex flex-col">
+
+              <div className="card ml-12 flex flex-col">
                 <h1 className="text-white text-xl">
                   4 billion years ago
                 </h1>
@@ -56,18 +58,20 @@ const HorizontalScrollCarousel = () => {
                 </p>
               </div>
               {/* Earth info C */}
-              <div className="absolute bottom-1/3 left-[150vw] flex flex-row w-[70vw] items-center">
+                            <div className="h-[80vh] w-[40vw] ml-12">
+              <div className="flex flex-row w-[40vw] ml-20 items-center">
                 <motion.div style={{ y: yEarthB }}>
                   <img src={Earth} className="h-[300px] w-[300px]" />
                 </motion.div>
               </div>
               <motion.div
-                className="absolute bottom-[3/4] left-[145vw] transform w-[200px] h-[400px]"
+                className="flex items-end w-[200px] h-[400px]"
                 style={{ y: yMoon }}
               >
-                <Spline scene="https://prod.spline.design/LzhVNa1synAwhP-C/scene.splinecode" />
+                <Spline scene="https://prod.spline.design/LzhVNa1synAwhP-C/scene.splinecode"/>
               </motion.div>
-              <div className="card absolute left-[100vw] ml-12 flex flex-col">
+                            </div>
+              <div className="card flex flex-col">
                 <h1 className="text-white text-xl">
                   4 billion years ago
                 </h1>
@@ -76,9 +80,9 @@ const HorizontalScrollCarousel = () => {
                 </p>
               </div>
               {/* Earth info D */}
-              <div className="absolute bottom-[20vh] left-[270vw] flex flex-row w-[70vw] items-center">
+              <div className="absolute bottom-[30vh] left-[200vw] flex flex-row w-[70vw] items-center">
                 <motion.div style={{ y: yEarthB }}>
-                    <img src={Earth} className="h-[800px] w-[800px]" />
+                    <img src={Earth} className="h-[95%] w-[95%]"/>
                 </motion.div>
               </div>
             </div>
